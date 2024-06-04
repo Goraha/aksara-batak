f958bfe2386efdada9039ae5d315a24de97fe868
GA-Purba <gabrielandrianpurba@gmail.com>
2024-06-04 22:59:47 +0700

    add font

--- src/Pages/home-menu.jsx
+++ src/Pages/home-menu.jsx
@@ -3,7 +3,7 @@ const HomeMenuPage = () => {
 	return(
 		<div className="flex flex-col h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
 				<div className="flex item justify-center w-full">
-					<h1 className="text-3xl font-bold mb-2 text-white my-2">Aksara Batak</h1>
+					<h1 className="text-3xl font-pacifico font-bold mb-2 text-white my-2">Aksara Batak</h1>
 				</div>
 				<div className="flex w-full h-full items-center justify-center">
 					<div className="w-1/2">
@@ -12,25 +12,25 @@ const HomeMenuPage = () => {
 							transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 
 							hover:scale-110 hover:bg-indigo-900 duration-300"
 							onClick={() => {window.location.href = "/aksara-simalungun"}}>
-								<p className="text-white text-md">Aksara Toba</p>
+								<p className="text-white text-md font-rubik">Toba</p>
 							</div>
 							<div className="flex items-center justify-center h-32 rounded-md bg-indigo-950 cursor-pointer 
 							transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 
 							hover:scale-110 hover:bg-indigo-900 duration-300"
 							onClick={() => {window.location.href = "/aksara-simalungun"}}>
-								<p className="text-white text-md">Aksara Simalungun</p>
+								<p className="text-white text-md font-rubik">Simalungun</p>
 							</div>
 							<div className="flex items-center justify-center h-32 rounded-md bg-indigo-950 cursor-pointer 
 							transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 
 							hover:scale-110 hover:bg-indigo-900 duration-300"
 							onClick={() => {window.location.href = "/aksara-simalungun"}}>
-								<p className="text-white text-md">Aksara Karo</p>
+								<p className="text-white text-md font-rubik">Karo</p>
 							</div>
 							<div className="flex items-center justify-center h-32 rounded-md bg-indigo-950 cursor-pointer 
 							transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 
 							hover:scale-110 hover:bg-indigo-900 duration-300"
 							onClick={() => {window.location.href = "/aksara-simalungun"}}>
-								<p className="text-white text-md">Aksara Angkola</p>
+								<p className="text-white text-md font-rubik">Angkola</p>
 							</div>
 						</div>
 					</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      pacifico: ["Pacifico", "sans-serif"],
+      rubik: ["Rubik", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
